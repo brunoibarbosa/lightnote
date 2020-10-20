d38a7dcd320c20b52ed160781ec64c8c658acc1d
Bruno Inacio <brunoinacio1590@gmail.com>
2020-10-20 14:50:30 -0300

    Página->Sobre criada

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './frontend/main.js',
     output: {
         path: path.resolve(__dirname, 'public', 'js'),
